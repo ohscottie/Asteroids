265817888ee43d47ae5162a15cbb60ff3f6b6bba
scottie <183048101+ohscottie@users.noreply.github.com>
2025-08-09 12:21:49 -0700

    feat: add player with triangle rendering

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
@@ -13,6 +18,8 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000.0
 
