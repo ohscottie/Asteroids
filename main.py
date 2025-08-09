3175249e05535dc0d93d20ffe9581caa92bc8115
scottie <183048101+ohscottie@users.noreply.github.com>
2025-08-09 11:52:54 -0700

    feat: add FPS limit and delta time tracking

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
+
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000.0
 
 
 if __name__ == "__main__":
