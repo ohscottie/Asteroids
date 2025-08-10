a08f42c960fefd7f3b2c61d3c42be9b9d25f5876
scottie <183048101+ohscottie@users.noreply.github.com>
2025-08-09 17:29:48 -0700

    feat: add player rotation controls

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
-
         pygame.display.flip()
         dt = clock.tick(60)/1000.0
 
